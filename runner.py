d52f16e2e2951ea5a42804ead436ca640b3b55e1
dkeriazisStuy <dkeriazis@stuy.edu>
2018-09-29 23:35:59 -0400

    Added support for args

--- runner.py
+++ runner.py
@@ -5,6 +5,7 @@ import docker
 import shutil
 import os
 import os.path
+import json
 
 client = docker.from_env()
 
@@ -18,23 +19,24 @@ def pull_image(image_name):
         name, tag = image_name.split(':')
         client.images.pull(name, tag=tag)
 
-def get_dockerfile(py_file):
+def get_dockerfile(cmd):
     return '''
         FROM python:3.7-alpine
         COPY . /root
         WORKDIR /root
-        CMD ["python", "{file}"]
-    '''.format(file=py_file)
+        CMD {cmd}
+    '''.format(cmd=cmd)
 
-def make_dockerfile(temp_dir, py_file):
+def make_dockerfile(temp_dir, py_file, arg_list):
     _, path = tempfile.mkstemp(dir=temp_dir)
     with open(path, 'w') as f:
-        dockerfile = get_dockerfile(py_file)
+        cmd = ["python", py_file] + arg_list
+        dockerfile = get_dockerfile(json.dumps(cmd))
         f.write(dockerfile)
         return path
 
 def build_image(d, filename, args, debug):
-    dockerfile_path = make_dockerfile(d, filename)
+    dockerfile_path = make_dockerfile(d, filename, args)
     shutil.copy(filename, d)
     image, stream = client.images.build(
             path=d,
@@ -60,18 +62,25 @@ def run_container(d, image):
 def get_stream(container):
     return container.logs(stream=True)
 
-def teardown(container, image):
-    container.remove()
-    client.images.remove(image=image.id)
+def teardown(image, container):
+    if container is not None:
+        container.remove()
+    if image is not None:
+        client.images.remove(image=image.id)
 
 def run_file(filename, args=(), debug=False):
-    pull_image('python:3.7-alpine')
-    d = tempfile.mkdtemp(dir=os.path.abspath('.'))
-    image = build_image(d, filename, args, debug)
-    container = run_container(d, image)
-    for s in get_stream(container):
-        print(s.decode('utf8'), end='')
-    teardown(container, image)
+    image = None
+    container = None
+    try:
+        pull_image('python:3.7-alpine')
+        d = tempfile.mkdtemp(dir=os.path.abspath('.'))
+        image = build_image(d, filename, args, debug)
+        container = run_container(d, image)
+        for s in get_stream(container):
+            print(s.decode('utf8'), end='')
+    finally:
+        teardown(image, container)
 
-run_file('baz.py', debug=True)
+if __name__ == "__main__":
+    run_file('_baz.py', args=['in_file.txt', 'out_file.txt'], debug=True)
 
