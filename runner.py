0e4df1d9e9e1aa5bb62f9fd4da810f5cdc123c35
dkeriazisStuy <dkeriazis@stuy.edu>
2018-09-30 13:08:53 -0400

    Added more tests and refactored

--- runner.py
+++ runner.py
@@ -20,24 +20,25 @@ def pull_image(image_name, debug):
         name, tag = image_name.split(':')
         client.images.pull(name, tag=tag)
 
-def get_dockerfile(cmd):
+def get_dockerfile(image, cmd):
     return '''
-        FROM python:3.7-alpine
+        FROM {image}
         COPY . /root
         WORKDIR /root
         CMD {cmd}
-    '''.format(cmd=cmd)
+    '''.format(image=image, cmd=cmd)
 
-def make_dockerfile(temp_dir, py_file, arg_list):
+def make_dockerfile(temp_dir, image, py_file, arg_list):
     _, path = tempfile.mkstemp(dir=temp_dir)
     with open(path, 'w') as f:
         cmd = ["python", py_file] + arg_list
-        dockerfile = get_dockerfile(json.dumps(cmd))
+        dockerfile = get_dockerfile(image, json.dumps(cmd))
         f.write(dockerfile)
         return path
 
-def build_image(d, filename, args, debug):
-    dockerfile_path = make_dockerfile(d, filename, args)
+def build_image(d, image, filename, args, debug):
+    pull_image(image, debug)
+    dockerfile_path = make_dockerfile(d, image, filename, args)
     shutil.copy(filename, d)
     image, stream = client.images.build(
             path=d,
@@ -60,9 +61,6 @@ def run_container(d, image):
         )
     return container
 
-def get_stream(container):
-    return container.logs(stream=True)
-
 def teardown(image, container):
     if container is not None:
         try:
@@ -74,7 +72,8 @@ def teardown(image, container):
         client.images.remove(image=image.id)
 
 def container_stream(container):
-    for s in get_stream(container):
+    stream = container.logs(stream=True)
+    for s in stream:
         print(s.decode('utf8'), end='')
 
 def start_container_stream(container, timeout):
@@ -94,9 +93,8 @@ def run_file(filename, args=None, timeout=10, debug=False):
     image = None
     container = None
     try:
-        pull_image('python:3.7-alpine', debug)
         d = tempfile.mkdtemp(dir=os.path.abspath('.'))
-        image = build_image(d, filename, args, debug)
+        image = build_image(d, 'python:3.7-alpine', filename, args, debug)
         container = run_container(d, image)
         start_container_stream(container, timeout)
     finally:
@@ -104,5 +102,6 @@ def run_file(filename, args=None, timeout=10, debug=False):
 
 if __name__ == "__main__":
     #  run_file('_baz.py', args=['in_file.txt', 'out_file.txt'], debug=True)
-    run_file('_loop.py', debug=True)
+    #  run_file('_loop.py', debug=True)
+    run_file('_for.py', debug=True)
 
